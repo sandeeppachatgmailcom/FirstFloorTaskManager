8237131cd9de60c876f4c083acf4f4f770fe7dab
sandeep <sandeepcheriyan@gmail.com>
2024-10-26 06:22:39 +0530

    swagger documentation server path added

--- documentation/swagger.js
+++ documentation/swagger.js
@@ -10,7 +10,7 @@ const swaggerOptions = {
       },
       servers: [
         {
-          url: 'http://localhost:5000',
+          url: 'https://firstfloortaskmanager.onrender.com',
         },
       ],
     },
